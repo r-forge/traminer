72a66e3d637659cfe8e8745df897be0b9e51adba
gritschard (Gilbert Ritschard) <gilbert.ritschard@unige.ch>
2022-10-07 11:28:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@1345 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineR/src/init.c
+++ pkg/TraMineR/src/init.c
@@ -13,7 +13,7 @@ extern SEXP checktriangleineq(SEXP, SEXP, SEXP);
 extern SEXP cstringdistance(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP cstringrefseqdistance(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP dist2matrix(SEXP, SEXP);
-extern SEXP getTraMineRDebugLevel();
+extern SEXP getTraMineRDebugLevel(void);
 extern SEXP setTraMineRDebugLevel(SEXP);
 extern SEXP tmrChisq(SEXP, SEXP, SEXP);
 extern SEXP tmrChisqRef(SEXP, SEXP, SEXP, SEXP);
