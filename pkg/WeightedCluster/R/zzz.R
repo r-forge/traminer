86f591b3cc30f1e11be2109b4e39f2de49f5aebe
matthias_studer (Matthias Studer) <matthias.studer@unige.ch>
2024-12-10 10:31:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@1463 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WeightedCluster/R/zzz.R
+++ pkg/WeightedCluster/R/zzz.R
@@ -13,10 +13,10 @@
 		builtDate <- ""
 	}
 	packageStartupMessage("This is WeightedCluster ", state, " version ", descr$Version, builtDate)
-	packageStartupMessage('\nTo get the manuals, please run:')
-	packageStartupMessage('   vignette("WeightedCluster") ## Complete manual in English')
-	packageStartupMessage('   vignette("WeightedClusterFR") ## Complete manual in French')
-	packageStartupMessage('   vignette("WeightedClusterPreview") ## Short preview in English')
+	packageStartupMessage('\nTo access available manuals and short tutorials, please run:')
+	packageStartupMessage('   vignette("WeightedCluster") ## For the complete manual in English')
+	packageStartupMessage('   vignette(package="WeightedCluster") ## To list available documentation')
+	#packageStartupMessage('   vignette("WeightedClusterPreview") ## Short preview in English')
 	packageStartupMessage("\nTo cite WeightedCluster in publications please use:")
 	x <- "Studer, Matthias (2013). WeightedCluster Library Manual: A practical guide to creating typologies of trajectories in the social sciences with R. LIVES Working Papers, 24. doi: 10.12682/lives.2296-1658.2013.24"
 	sapply(strwrap(x, exdent=3), packageStartupMessage)
