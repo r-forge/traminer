c036daf9b4cc18351fd3ae5347880cf0f54ee90f
matthias_studer (Matthias Studer) <matthias.studer@unige.ch>
2024-12-10 10:52:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@1464 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WeightedCluster/R/zzz.R
+++ pkg/WeightedCluster/R/zzz.R
@@ -17,7 +17,7 @@
 	packageStartupMessage('   vignette("WeightedCluster") ## For the complete manual in English')
 	packageStartupMessage('   vignette(package="WeightedCluster") ## To list available documentation')
 	#packageStartupMessage('   vignette("WeightedClusterPreview") ## Short preview in English')
-	packageStartupMessage("\nTo cite WeightedCluster in publications please use:")
+	packageStartupMessage("\nTo cite WeightedCluster in publications please use or references in the help pages:")
 	x <- "Studer, Matthias (2013). WeightedCluster Library Manual: A practical guide to creating typologies of trajectories in the social sciences with R. LIVES Working Papers, 24. doi: 10.12682/lives.2296-1658.2013.24"
 	sapply(strwrap(x, exdent=3), packageStartupMessage)
 	invisible()
