d85c4eea07f20deacbb4a4da7d4c14d452de2391
matthias_studer (Matthias Studer) <matthias.studer@unige.ch>
2025-02-24 10:12:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@1469 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WeightedCluster/R/wfcmdd.r
+++ pkg/WeightedCluster/R/wfcmdd.r
@@ -5,120 +5,137 @@ wfcmdd <- function(diss, memb, weights = NULL, method = "FCMdd",
   	METHODS <- c("NCdd", "HNCdd", "FCMdd", "PCMdd")
   	method <- match.arg(method, METHODS)
 
-  	pweights <- weights/sum(weights)
-	  d <- as.matrix(diss)
-	  n <- nrow(d)
-	  if (method == "NCdd") {
+	#pweights <- weights/sum(weights)
+	mexp <- -(1 / (m - 1))
+	d <- as.matrix(diss)
+	n <- nrow(d)
+	## Checking weights
+	if(is.null(weights)){
+		weights <- rep(1, n)
+	}
+	## Basic arguments check by method
+	if (method == "NCdd") {
 	    if (is.null(dnoise)&&is.null(dlambda)) {
-	      stop("Must provide a value for dnoise or dlambda")
+			stop("Must provide a value for dnoise or dlambda")
 	    }
 	  	if(!is.null(dlambda)){
 	  		dnoise <- 1
-	  	}
-  	} else if (method == "HNCdd") {
-		    if (is.null(dnoise)) {
-		      stop("Must provide a value for dnoise")
-		    }
-		    m <- 1
-	  } else if (method == "PCMdd") {
-		    if (is.null(eta)) {
-		      stop("Must provide a vector of values for eta")
-		    }
-	  }
-	  
-	  if (is.data.frame(memb) || is.matrix(memb)) {
-		    if (nrow(memb) != ncol(d)) {
-		      	stop("The number of rows in memb must be the same as the number rows and columns of d")
-		    }
-		    u <- as.matrix(memb)
-	  } else if (is.vector(memb) && is.numeric(memb)) {
-		    u <- matrix(0, n, length(memb))
-		    for (k in 1:length(memb)) {
-		      	u[memb[k], k] <- 1
-		    }
-	  } else {
-	    	stop("Provide a number, a vector of seeds, or membership matrix for mobile clusters")
-	  }
+		}
+	} else if (method == "HNCdd") {
+		if (is.null(dnoise)) {
+			stop("Must provide a value for dnoise")
+		}
+		m <- 1
+	} else if (method == "PCMdd") {
+		if (is.null(eta)) {
+			stop("Must provide a vector of values for eta")
+		}
+	}
+	## Initialization of memberhsip matrix
+	if (is.data.frame(memb) || is.matrix(memb)) {
+		if (nrow(memb) != ncol(d)) {
+			stop("The number of rows in memb must be the same as the number rows and columns of d")
+		}
+		u <- as.matrix(memb)
+	} else if (is.vector(memb) && is.numeric(memb)) {
+		u <- matrix(0, n, length(memb))
+		for (k in 1:length(memb)) {
+			u[memb[k], k] <- 1
+		}
+	} else {
+		stop("Provide a number, a vector of seeds, or membership matrix for mobile clusters")
+	}
 	
-	  kMov <- ncol(u)
-	  med <- rep(NA, kMov)
-	  if ((method == "PCMdd") && length(eta) != kMov) {
-	    	stop("Vector of reference distances (eta) must have a length equal to the number of clusters")
-	  }
-	  if (method == "NCdd" || method == "HNCdd") {
-	    	u <- cbind(u, vector("numeric", length = n))
-	  }
-	  kMovNC <- ncol(u)
-	  uPrev <- matrix(0, nrow = n, ncol = kMovNC)
-	  if(!is.null(dlambda)) kdiv <- kMov*sum(weights)
-	  dist2med <- matrix(0, nrow = n, ncol = kMovNC)
-	  if (method == "NCdd" || method == "HNCdd") {
-	  		dist2med[, kMovNC] <- dnoise
-	  }
+	kMov <- ncol(u)
+	if ((method == "PCMdd") && length(eta) != kMov) {
+		stop("Vector of reference distances (eta) must have a length equal to the number of clusters")
+	}
+	
+	if (method == "NCdd" || method == "HNCdd") {
+		u <- cbind(u, vector("numeric", length = n))
+	}
+	kMovNC <- ncol(u)
+	med <- rep(NA, kMov)
+	uPrev <- matrix(0, nrow = n, ncol = kMovNC)
+	dist2med <- matrix(0, nrow = n, ncol = kMovNC)
+	
+	## Dynamic lambda, for noise clustering Compute only once
+	if(!is.null(dlambda)) kdiv <- kMov*sum(weights)
+	
+	## Adding the noise column
+	if (method == "NCdd" || method == "HNCdd") {
+		dist2med[, kMovNC] <- dnoise
+	}
   	continue <- TRUE
   	iter <- 1
   	while (continue) {
+
   		##Finding centers
     	for (k in 1:kMov) {
+			## Do not consider medoids of other clusters
       		candidates <- which(apply(u[, -k, drop = FALSE], 1, max) < 1 & (!1:n %in% med[0:(k - 1)]))
       		med[k] <- candidates[which.min((u[, k]^m * weights) %*% d[, candidates])]
       		dist2med[, k] <- d[, med[k]]
     	}
   		#updating dnoise for adaptative dnoise clustering.
 	  	if(!is.null(dlambda) & method=="NCdd"){
-	  			dnoise <- dlambda*sum(dist2med[, -kMovNC, drop=F]*weights)/(kdiv)
-	  			dist2med[, kMovNC] <- dnoise
+			dnoise <- dlambda*sum(dist2med[, -kMovNC, drop=F]*weights)/(kdiv)
+			dist2med[, kMovNC] <- dnoise
 	  	} 
   		## Updating membership
     	if (method == "HNCdd") {
-		      d2cm <- cbind(dist2med, dnoise)
-		      u[, ] <- 0
-		      minC <- apply(d2cm, 1, which.min)
-		      for (k in 1:length(minC)) {
-		    	    u[k, minC[k]] <- 1
-		      }
+			#d2cm <- cbind(dist2med, dnoise)
+			u[, ] <- 0
+			minC <- apply(dist2med, 1, which.min)
+			for (k in 1:length(minC)) {
+				u[k, minC[k]] <- 1
+			}
     	} else if (method %in% c("FCMdd", "NCdd")) {
-    			u <- (1/dist2med)^(1/(m-1))
-    			u <- u/rowSums(u)
-    			u[dist2med==0] <- 1
-      } else if (method == "PCMdd") {
+			zerodistcond <- dist2med==0
+			allmed <- rowSums(zerodistcond)>0 #Check any object, with a zero distance=> medoids
+			u <- dist2med^mexp
+			u[allmed, ] <- 0 ## Medoids have 0 membership to all clusters
+			u[zerodistcond] <- 1 ## Except their own.
+			u <- u/rowSums(u)
+		} else if (method == "PCMdd") { ## Unsure
       		for (k in 1:ncol(dist2med)) {
-        			u[, k] <- 1 / (1 + ((dist2med[ ,k]) / eta[k])^(1 / (m - 1)))
+				u[, k] <- 1 / (1 + ((dist2med[ ,k]) / eta[k])^(-mexp))
       		}
       		u[dist2med == 0] <- 1
     	}
 	    if (iter > 2) {
-	      	continue <- (max(abs(u - uPrev)) > alpha) && (iter <=iter.max) && 
+	      	continue <- (iter <=iter.max) && (max(abs(u - uPrev)) > alpha)# && 
 	      							(max(abs(u - uPrev2)) > alpha)
 	    }
 	    if (continue) {
-		      uPrev2 <- uPrev
-		      uPrev <- u
-		      iter <- iter + 1
-		      if (verbose) {
-		    	    cat(".")
-		      }
+			uPrev2 <- uPrev
+			uPrev <- u
+			iter <- iter + 1
+			if (verbose) {
+				cat(".")
+			}
 	    }
   	}
   	if (method %in% c("NCdd", "FCMdd")) {
-    		functional <- sum(dist2med * (u^m) * weights)
+		functional <- sum(dist2med * (u^m) * weights)
   	} else if (method == "HNCdd") {
-    		functional <- sum(dist2med * (u^m * weights))
+		functional <- sum(dist2med * (u^m * weights))
   	} else if (method == "PCMdd") {
-    		functional <- 0
-		    for (k in 1:(kMov + 1)) {
-		      functional <- functional +
-		        sum(dist2med[, k] * (u[, k]^m) * weights) + sum(eta[k] *
-		          (1 - u[, k])^m * weights)
-		    }
+		functional <- 0
+		for (k in 1:(kMov + 1)) {
+		  functional <- functional +
+			sum(dist2med[, k] * (u[, k]^m) * weights) + sum(eta[k] *
+			  (1 - u[, k])^m * weights)
+		}
   	}
   	if (verbose) {
-    		cat("\nIterations:", iter, "Functional: ",functional, "\n")
+		cat("\nIterations:", iter, "Functional: ",functional, "\n")
   	}
 
     mobileCenters <- med[1:kMov]
-	  res <- list(dnoise = dnoise, memb = u, mobileCenters = mobileCenters,
-	  						functional = functional)
+	res <- list(dnoise = dnoise, memb = u, 
+				mobileCenters = mobileCenters,
+				functional = functional)
   return(res)
 }
 
